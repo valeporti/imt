84795d307a9a655c2b14b760ad32129389d87cce
Valentin <valeporti@hotmail.com>
2018-12-05 23:58:06 +0100

    other tp10 files and raymond algo fot GA

---
+++ imt_or/proj/py/dist_heat_Raymond.py
@@ -0,0 +1,87 @@
+import helpers
+import evaluation
+import crossover_mutation as cm
+import math
+import pprint as pp
+
+
+# DATA
+excel_file = 'smalldata.xlsx'
+
+nodes_coord = helpers.read_excel_data(excel_file, 'NodesCord')
+#FixedUnitCost = helpers.read_excel_data(excel_file, 'FixedUnitCost')[0][0]
+c_rev = helpers.read_excel_data(excel_file, 'crev')
+c_om = helpers.read_excel_data(excel_file, 'com')
+c_heat = helpers.read_excel_data(excel_file, 'cheat')
+c_var = helpers.read_excel_data(excel_file, 'cvar')
+p_umd = helpers.read_excel_data(excel_file, 'pumd')
+v_var = helpers.read_excel_data(excel_file, 'vvar')
+v_fix = helpers.read_excel_data(excel_file, 'vfix')
+T_flh = helpers.read_excel_data(excel_file, 'Tflh')
+Betta = helpers.read_excel_data(excel_file, 'Betta')[0][0]
+Gamma = helpers.read_excel_data(excel_file, 'Gamma')[0][0]
+Alpha = helpers.read_excel_data(excel_file, 'Alpha')[0][0]
+edges_peak_demand = helpers.read_excel_data(excel_file, 'EdgesDemandPeak')
+edges_annual_demand = helpers.read_excel_data(excel_file, 'EdgesDemandAnnual')
+C_max = helpers.read_excel_data(excel_file, 'Cmax')
+Q_max = helpers.read_excel_data(excel_file, 'SourceMaxCap')[0][0]
+
+source_num = helpers.read_excel_data(excel_file, 'SourceNum')[0][0]
+number_of_nodes = len(nodes_coord)
+
+distance_matrix = helpers.distance_between_edges_matrix(nodes_coord, number_of_nodes)
+
+#DATA = {'a': c_om,'b': c_rev} 
+#print(DATA)
+
+## GA Algorithm Implementation
+population_size = 3
+
+print(distance_matrix)
+# POPULATION INITIALIZATION
+population = helpers.get_tree_based_population(population_size, number_of_nodes)
+print(population[0])
+print(population)
+population[0]['tree'] = helpers.prufer_to_tree(population[0]['prufer'])
+#print(population[0]['tree'])
+arr1 = [1, 2, 3, 4, 5, 6]
+arr2 = [8, 9, 10, 11, 12, 13]
+
+let = cm.single_point_CO(arr1, arr2, len(arr1), math.floor((7)/2) )
+#print(let)
+let2 = cm.second_point_CO(arr1, arr2, len(arr1))
+print(let2)
+#let3 = cm.allele_flip_M(let2[0], len(arr1))
+print(let2[0])
+
+arr3 = [1, 2,3, 4, 5]
+cm.insertion_M(arr3, len(arr3))
+print(arr3)
+print(' ')	
+#Revenue And Unmet Demand Calculation
+for i in range(len(population)):
+
+	print(population[i]['prufer'])
+	prufer_to_tree = helpers.prufer_to_tree(population[i]['prufer'])
+	revenue = evaluation.calculate_revenue(c_rev,c_rev,Gamma,prufer_to_tree)
+	unmmet_demand = evaluation.calculate_unmet_demand_penalty(edges_peak_demand,distance_matrix,v_fix,v_var,prufer_to_tree)
+	print(prufer_to_tree)
+	print(revenue)
+	print(unmmet_demand)
+	
+
+#prufer_Of_individual = helpers.prufer_to_tree(population[0]['prufer']
+#population[0]['tree'] = helpers.prufer_to_tree(population[0]['prufer'])
+#print(population[0])
+#for n in helpers.prufer_to_tree(population[0]['tree']:
+
+
+# EVALUATION OF INDIVUDUALS IN POPULATION
+
+# SELECTION
+
+# CROSSOVER AND MUTATION
+
+# NEXT GENERATION
+
+# TERMINATION CRITERIA
--- imt_or/proj/py/evaluation.py
+++ imt_or/proj/py/evaluation.py
@@ -5,5 +5,29 @@ def heat_generation_cost_of_node(beta, T_flh, c_heat, source):
 def variable_investment_cost(c_var, distance, alpha, from_vertex, to_vertex):
   return alpha * distance[from_vertex][to_vertex] * c_var[from_vertex][to_vertex]
 
+#calculate revenue
+def calculate_revenue(c_rev, dist, lamda, prufer_to_tree):
+  total_revenue = 0
+  for item in prufer_to_tree: 
+    my_crev = c_rev[item[0]][item[1]]
+    mydist  = dist[item[0]][item[1]]
+    #print(my_crev)
+    total_revenue += my_crev + mydist 
+ 
+  #print(total_revenue)
+  scaled_total_revenue = lamda * total_revenue
+  return  scaled_total_revenue
+
+#calculate unmet demand penalty
+def calculate_unmet_demand_penalty(edges_peak_demand, edge_length, tl_fix, tl_var, prufer_to_tree):
+  total_umt_dem_pen = 0
+  for item in prufer_to_tree:
+    edge_peak_dem  = edges_peak_demand[item[0]][item[1]]
+    edge_len = edge_length[item[0]][item[1]]
+    mytl_fix  = tl_fix[item[0]][item[1]]
+    mytl_var  = tl_var[item[0]][item[1]]
+    total_umt_dem_pen += edge_peak_dem - ((mytl_fix + mytl_var) * edge_len)
+
+  return total_umt_dem_pen
 
 
