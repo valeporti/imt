7d11a0a8897f9c43e88da21e3e15b094f18b78a7
Valentin <valeporti@hotmail.com>
2018-12-13 18:53:31 +0100

    ended localization analysis

--- imt_signal/Matlab files/Localization/localization.m
+++ imt_signal/Matlab files/Localization/localization.m
@@ -2,15 +2,23 @@ clear all; clc; close all;
 
 load('signaux_localisation.mat')
 
-figure
-plot(signal_BS1)
-figure
-plot(signal_BS2)
-figure
-plot(signal_BS3)
 [r, lags] = xcorr(signal_ref, signal_BS1);
-
 figure
-plot(lags, r)
+plot(lags, r);
 [~,I] = max(abs(r));
-lagDiff = lags(I)
+lagDiff_SigBS1 = abs(lags(I)); % in micro seconds
+[r, lags] = xcorr(signal_ref, signal_BS2);
+figure
+plot(lags, r);
+[~,I] = max(abs(r));
+lagDiff_SigBS2 = abs(lags(I)); % in micro seconds
+[r, lags] = xcorr(signal_ref, signal_BS3);
+figure
+plot(lags, r);
+[~,I] = max(abs(r));
+lagDiff_SigBS3 = abs(lags(I)); % in micro seconds
+
+speed_of_light = physconst('LightSpeed'); % m/s
+distance_BS1 = ((lagDiff_SigBS1 / 1000000) * speed_of_light) / 1000 % in km 
+distance_BS2 = ((lagDiff_SigBS2 / 1000000) * speed_of_light) / 1000 % in km
+distance_BS3 = ((lagDiff_SigBS3 / 1000000) * speed_of_light) / 1000 % in km
