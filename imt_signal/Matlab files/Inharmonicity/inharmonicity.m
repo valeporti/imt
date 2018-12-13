3bca7a986747440b1bf7e2e1e565aba9cf464e4d
Valentin <valeporti@hotmail.com>
2018-12-13 21:59:23 +0100

    final changes for delivery

--- imt_signal/Matlab files/Inharmonicity/inharmonicity.m
+++ imt_signal/Matlab files/Inharmonicity/inharmonicity.m
@@ -15,20 +15,12 @@ plot(line_vector, X_ffts_log)
 
 %Q.2.1
 freq_on_graph = [220.5 442.1 663.3 885.6 1108 1332 1556];
-n = length(freq_on_graph) - 1;
-freq_diff = zeros(n);
-sum = 0;
+n = length(freq_on_graph);
+E_n = zeros(n);
 for i=1:n
-    freq_diff(i) = freq_on_graph(i+1) - freq_on_graph(i);
-    sum = sum + freq_diff(i);
-end
-f_1 = sum / n
-
-E_n = zeros(n + 1);
-for i=1:n+1
     E_n(i) = 1200 * (log2(freq_on_graph(i)) - log2(i * freq_on_graph(1)));
 end
-
+f_1 = freq_on_graph(1)
 E_n
 
 [x,Fs]=audioread('piano2.wav');
@@ -46,20 +38,12 @@ plot(line_vector, X_ffts_log)
 
 %Q.2.3
 freq_on_graph = [217.9 443 668.1 892.7 1118 1347 1572];
-n = length(freq_on_graph) - 1;
-freq_diff = zeros(n);
-sum = 0;
+n = length(freq_on_graph);
+E_n = zeros(n);
 for i=1:n
-    freq_diff(i) = freq_on_graph(i+1) - freq_on_graph(i);
-    sum = sum + freq_diff(i);
-end
-f_1 = sum / n
-
-E_n = zeros(n + 1);
-for i=1:n+1
     E_n(i) = 1200 * (log2(freq_on_graph(i)) - log2(i * freq_on_graph(1)));
 end
-
+f_1 = freq_on_graph(1)
 E_n
 
 
--- imt_signal/Matlab files/Localization/localization.m
+++ imt_signal/Matlab files/Localization/localization.m
@@ -6,27 +6,29 @@ load('signaux_localisation.mat')
 figure
 plot(lags, r);
 [~,I] = max(abs(r));
-lagDiff_SigBS1 = abs(lags(I)); % in 10-7 seconds
+lagDiff_SigBS1 = abs(lags(I)) % in 10-7 seconds
 [r, lags] = xcorr(signal_ref, signal_BS2);
 figure
 plot(lags, r);
 [~,I] = max(abs(r));
-lagDiff_SigBS2 = abs(lags(I)); % in 10-7 seconds
+lagDiff_SigBS2 = abs(lags(I)) % in 10-7 seconds
 [r, lags] = xcorr(signal_ref, signal_BS3);
 figure
 plot(lags, r);
 [~,I] = max(abs(r));
-lagDiff_SigBS3 = abs(lags(I)); % in 10-7 seconds
+lagDiff_SigBS3 = abs(lags(I)) % in 10-7 seconds
 
 speed_of_light = physconst('LightSpeed'); % m/s
-distance_BS1 = ((lagDiff_SigBS1 / 10000000) * speed_of_light) / 1000; % in km 
-distance_BS2 = ((lagDiff_SigBS2 / 10000000) * speed_of_light) / 1000; % in km
-distance_BS3 = ((lagDiff_SigBS3 / 10000000) * speed_of_light) / 1000; % in km
+distance_BS1 = ((lagDiff_SigBS1 / 10000000) * speed_of_light) / 1000 % in km 
+distance_BS2 = ((lagDiff_SigBS2 / 10000000) * speed_of_light) / 1000 % in km
+distance_BS3 = ((lagDiff_SigBS3 / 10000000) * speed_of_light) / 1000 % in km
 
 [x_out_12, y_out_12] = circcirc(0, 0, distance_BS1, 4, 2, distance_BS2)
 [x_out_13, y_out_13] = circcirc(0, 0, distance_BS1, 1, 4, distance_BS3)
 [x_out_23, y_out_23] = circcirc(4, 2, distance_BS2, 1, 4, distance_BS3)
 
+
+% circles method
 figure
 circle(0, 0, distance_BS1)
 hold on
