d3e4f17b25ca3a496e6a7c2e7f1f46482f34add7
Valentin <valeporti@hotmail.com>
2018-12-13 19:25:55 +0100

    added circles calculation in order to get the aproximative location

--- imt_signal/Matlab files/Localization/localization.m
+++ imt_signal/Matlab files/Localization/localization.m
@@ -6,19 +6,41 @@ load('signaux_localisation.mat')
 figure
 plot(lags, r);
 [~,I] = max(abs(r));
-lagDiff_SigBS1 = abs(lags(I)); % in micro seconds
+lagDiff_SigBS1 = abs(lags(I)); % in 10-7 seconds
 [r, lags] = xcorr(signal_ref, signal_BS2);
 figure
 plot(lags, r);
 [~,I] = max(abs(r));
-lagDiff_SigBS2 = abs(lags(I)); % in micro seconds
+lagDiff_SigBS2 = abs(lags(I)); % in 10-7 seconds
 [r, lags] = xcorr(signal_ref, signal_BS3);
 figure
 plot(lags, r);
 [~,I] = max(abs(r));
-lagDiff_SigBS3 = abs(lags(I)); % in micro seconds
+lagDiff_SigBS3 = abs(lags(I)); % in 10-7 seconds
 
 speed_of_light = physconst('LightSpeed'); % m/s
-distance_BS1 = ((lagDiff_SigBS1 / 1000000) * speed_of_light) / 1000 % in km 
-distance_BS2 = ((lagDiff_SigBS2 / 1000000) * speed_of_light) / 1000 % in km
-distance_BS3 = ((lagDiff_SigBS3 / 1000000) * speed_of_light) / 1000 % in km
+distance_BS1 = ((lagDiff_SigBS1 / 10000000) * speed_of_light) / 1000; % in km 
+distance_BS2 = ((lagDiff_SigBS2 / 10000000) * speed_of_light) / 1000; % in km
+distance_BS3 = ((lagDiff_SigBS3 / 10000000) * speed_of_light) / 1000; % in km
+
+[x_out_12, y_out_12] = circcirc(0, 0, distance_BS1, 4, 2, distance_BS2)
+[x_out_13, y_out_13] = circcirc(0, 0, distance_BS1, 1, 4, distance_BS3)
+[x_out_23, y_out_23] = circcirc(4, 2, distance_BS2, 1, 4, distance_BS3)
+
+figure
+circle(0, 0, distance_BS1)
+hold on
+circle(4, 2, distance_BS2)
+circle(1, 4, distance_BS3)
+hold off
+
+function circle(x,y,r)
+%x and y are the coordinates of the center of the circle
+%r is the radius of the circle
+%0.01 is the angle step, bigger values will draw the circle faster but
+%you might notice imperfections (not very smooth)
+ang=0:0.001:2*pi; 
+xp=r*cos(ang);
+yp=r*sin(ang);
+plot(x+xp,y+yp);
+end
